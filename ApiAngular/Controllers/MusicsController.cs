0f4893327cc8300ef9dbc32915496bde668e1d4d
huy <thuyhuy333@gmail.com>
2019-05-30 13:21:13 +0700

    single page application còn chưa edit được

---
+++ ApiAngular/Controllers/MusicsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiAngular.Models;
+
+namespace ApiAngular.Controllers
+{
+    public class MusicsController : ApiController
+    {
+        private readonly MusicDb _db = new MusicDb();
+
+        // GET: api/Musics  
+        public IQueryable<Music> GetMusics()
+        {
+            return _db.Musics;
+        }
+
+        // GET: api/Musics/5  
+        [ResponseType(typeof(Music))]
+        public IHttpActionResult GetMusic(int id)
+        {
+            var music = _db.Musics.Find(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(music);
+        }
+
+        // PUT: api/Musics/5  
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMusic(int id, Music music)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != music.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(music).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MusicExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Musics  
+        [ResponseType(typeof(Music))]
+        public IHttpActionResult PostMusic(Music music)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Musics.Add(music);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = music.Id }, music);
+        }
+
+        // DELETE: api/Musics/5  
+        [ResponseType(typeof(Music))]
+        public IHttpActionResult DeleteMusic(int id)
+        {
+            var music = _db.Musics.Find(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            _db.Musics.Remove(music);
+            _db.SaveChanges();
+
+            return Ok(music);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MusicExists(int id)
+        {
+            return _db.Musics.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ ApiAngular/MusicApp/Scripts/theMusic.js
@@ -0,0 +1,140 @@
+﻿(function () {
+    var app = angular.module("theMusic", ["ngRoute"]);
+    var config = function ($routeProvider) {
+        $routeProvider
+            .when("/list",
+                { templateUrl: "/MusicApp/Views/list.html", controller: "MusicListController" })
+            .when("/details/:id",
+                { templateUrl: "/MusicApp/Views/details.html", controller: "DetailsController" })
+            .otherwise(
+                { redirectTo: "/list", controller: "MusicListController" });
+    };
+
+    app.config(config);  
+    app.constant("musicApiUrl", "/api/musics/");  
+
+    //musicService
+    var musicService = function ($http, musicApiUrl) {
+        var getAll = function () {
+            return $http.get(musicApiUrl);
+        };
+
+        var getById = function (id) {
+            return $http.get(musicApiUrl + id);
+        };
+
+        var update = function (music) {
+            return $http.put(musicApiUrl + music.id, music);
+        };
+
+        var create = function (music) {
+            return $http.post(musicApiUrl, music);
+        };
+
+        var destroy = function (id) {
+            return $http.delete(musicApiUrl + id);
+        };
+
+        return {
+            getAll: getAll,
+            getById: getById,
+            update: update,
+            create: create,
+            delete: destroy
+        };
+    };
+    app.factory("musicService", musicService);  
+
+    //MusicListController
+    app.controller('MusicListController', function ($scope, musicService) {
+        musicService
+            .getAll()
+            .then(function (response) {
+                $scope.musics = response.data;
+            });  
+
+        $scope.delete = function (music) {
+            musicService.delete(music.Id)
+                .then(function () {
+                    removeMusicById(music.Id);
+                });
+        };
+
+        var removeMusicById = function (id) {
+            for (var i = 0; i < $scope.musics.length; i++) {
+                if ($scope.musics[i].Id == id) {
+                    $scope.musics.splice(i, 1);
+                    break;
+                }
+            }
+        };
+
+        $scope.create = function () {
+            $scope.edit = {
+                music: {
+                    Title: "",
+                    Singers: "",
+                    ReleaseDate: new Date,
+                    RunTime: 0
+                }
+            };
+        };  
+    
+    });
+
+    //DetailsController
+    app.controller("DetailsController", function ($scope, $routeParams, musicService) {
+        var id = $routeParams.id;
+        musicService
+            .getById(id)
+            .then(function (response) {
+                $scope.music = response.data;
+            });
+
+        $scope.edit = function () {
+            $scope.edit.music = angular.copy($scope.music);
+        }; 
+    });  
+
+    // EditController
+    app.controller("EditController", function ($scope, musicService) {
+
+        $scope.isEditable = function () {
+            return $scope.edit && $scope.edit.music;
+        };
+
+        $scope.cancel = function () {
+            $scope.edit.music = null;
+        };
+
+        $scope.save = function () {
+            if ($scope.edit.music.Id) {
+                updateMusic();
+            } else {
+                createMusic();
+            }
+        };
+
+        $scope.musics = [];
+        var updateMusic = function () {
+            musicService.update($scope.edit.music)
+                .then(function () {
+                    angular.extend($scope.music, $scope.edit.music);
+                    $scope.edit.music = null;
+                });
+        };
+
+        var createMusic = function () {
+            musicService.create($scope.edit.music)
+                .then(function () {
+                    $scope.musics.push($scope.edit.music);
+                    $scope.edit.music = null;
+                });
+        };
+
+    });
+   
+ 
+}());  
+
+ 
